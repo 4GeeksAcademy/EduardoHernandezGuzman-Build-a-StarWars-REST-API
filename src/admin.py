e700b4f7df4e4cacff8c32dd892e5022df212593
Edo <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2023-12-19 23:24:48 +0000

    Creados modelos planet_fav y character_fav

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from models import db, User , Character , Planet
+from models import db, User , Character , Planet , Character_fav , Planet_fav
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,6 +12,8 @@ def setup_admin(app):
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Character, db.session))
+    admin.add_view(ModelView(Character_fav, db.session))
     admin.add_view(ModelView(Planet, db.session))
+    admin.add_view(ModelView(Planet_fav, db.session))
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/models.py
+++ src/models.py
@@ -2,12 +2,20 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
+
+#############################USUARIOS###################################
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    #Relación con character_fav y planet_fav
+    character_fav = db.relationship("Character_fav", back_populates="user")
+    planet_fav = db.relationship("Planet_fav", back_populates="user")
+
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -20,7 +28,7 @@ class User(db.Model):
             # do not serialize the password, its a security breach
         }
 
-    
+#############################PERSONAJES################################### 
 class Character(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name =  db.Column(db.String(250), nullable=False)
@@ -28,6 +36,9 @@ class Character(db.Model):
     mass = db.Column(db.Float, nullable=False)
     hair_color = db.Column(db.String(250), nullable=False)
     skin_color = db.Column(db.String(250), nullable=False)
+
+    #Relación con character_fav
+    character_fav = db.relationship("Character_fav", back_populates="character")
 
     def __repr__(self):
         return '<Character %r>' % self.name
@@ -42,12 +53,26 @@ class Character(db.Model):
             "skin_color": self.skin_color,
         }
 
+class Character_fav(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    character_id = db.Column(db.Integer, db.ForeignKey("character.id"))
+    user = db.relationship("User", back_populates="character_fav")
+
+    #Relación con character
+    character = db.relationship("Character", back_populates="character_fav")
+
+
+#############################PLANETAS###################################
 class Planet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name =  db.Column(db.String(250), nullable=False)
     population = db.Column(db.Integer, nullable=False)
     terrain = db.Column(db.String(250), nullable=False)
     climate = db.Column(db.String(250), nullable=False)  
+
+    #Relación con planet_fav
+    planet_fav = db.relationship("Planet_fav", back_populates="planet")
 
     def __repr__(self):
         return '<Planet %r>' % self.name
@@ -60,3 +85,12 @@ class Planet(db.Model):
             "terrain": self.terrain,
             "climate": self.climate,
         }
+
+class Planet_fav(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    planet_id = db.Column(db.Integer, db.ForeignKey("planet.id"))
+    user = db.relationship("User", back_populates="planet_fav")
+
+    #Relación con planet
+    planet = db.relationship("Planet", back_populates="planet_fav")
